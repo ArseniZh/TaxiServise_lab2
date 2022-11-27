b6a4b68aada6563148bc50486180c8c9183efd2c
ArseniZh <szhirnevich@mail.ru>
2022-11-27 20:27:07 +0300

    add: new Comparator

---
+++ src/CompareFuelConsumption.java
@@ -0,0 +1,13 @@
+import java.util.Comparator;
+import cars.*;
+
+public class CompareFuelConsumption implements Comparator<Car> {
+    public int compare(Car o1, Car o2) {
+        if (o1.getFuelConsumption() > o2.getFuelConsumption()) {
+            return 1;
+        } else if (o1.getFuelConsumption() < o2.getFuelConsumption()) {
+            return -1;
+        }
+        return 0;
+    }
+}
