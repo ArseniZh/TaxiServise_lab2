d58a9f663652082c8514005f0cbf66ad8344dbad
ArseniZh <szhirnevich@mail.ru>
2022-11-27 20:20:33 +0300

    add: new child class of Car

---
+++ src/cars/PassengerCar.java
@@ -0,0 +1,7 @@
+package cars;
+
+public class PassengerCar extends Car{
+    public PassengerCar(String model, double speed, double weight, double cost, double fuelConsumption) {
+        super(model, speed, weight, cost, fuelConsumption);
+    }
+}
