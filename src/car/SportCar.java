fd0f4772bb7cb13d498b5ee0130a54e7d4552fa4
ArseniZh <szhirnevich@mail.ru>
2022-11-28 20:33:58 +0300

    feat (SportCar): add field acceleratingTime, getter, setter

--- src/car/SportCar.java
+++ src/car/SportCar.java
@@ -1,4 +1,18 @@
 package car;
 
-public class SportCar {
+public class SportCar extends Car {
+    private double acceleratingTime;
+    public SportCar(String model, double speed, double weight, double cost, double fuelConsumption, double acceleratingTime) {
+        super(model, speed, weight, cost, fuelConsumption);
+        this.acceleratingTime = acceleratingTime;
+    }
+
+    public void setAcceleratingTime(double acceleratingTime) {
+        this.acceleratingTime = acceleratingTime;
+    }
+    public double getAcceleratingTime() {
+        return this.acceleratingTime;
+    }
+
+
 }
