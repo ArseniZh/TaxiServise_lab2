2bf1de05072eb348b2161735bf7478b1308bc6ac
ArseniZh <szhirnevich@mail.ru>
2022-11-28 20:19:05 +0300

    add (PassengerCar): add field seatingPlace, setter and getter

--- src/car/PassengerCar.java
+++ src/car/PassengerCar.java
@@ -1,7 +1,18 @@
 package car;
 
 public class PassengerCar extends Car{
-    public PassengerCar(String model, double speed, double weight, double cost, double fuelConsumption) {
+    private int seatingPlace;
+    public PassengerCar(String model, double speed, double weight, double cost, double fuelConsumption, int seatingPlace) {
         super(model, speed, weight, cost, fuelConsumption);
+        this.seatingPlace = seatingPlace;
     }
+
+    public void setSeatingPlace(int seatingPlace) {
+        this.seatingPlace = seatingPlace;
+    }
+    public int getSeatingPlace() {
+        return this.seatingPlace;
+    }
+
+
 }
