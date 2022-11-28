7fdfbbb763f007c338b3bb98133bacd1b9489fa2
ArseniZh <szhirnevich@mail.ru>
2022-11-28 20:37:02 +0300

    fix (SportCar): overwrite toString and equals

--- src/car/SportCar.java
+++ src/car/SportCar.java
@@ -14,5 +14,15 @@ public class SportCar extends Car {
         return this.acceleratingTime;
     }
 
-
+    public String toString() {
+        return super.toString() + "\naccelerating time from 0 to 100(km/h): " + this.acceleratingTime + " sec";
+    }
+    public boolean equals(Object obj) {
+        if (super.equals(obj)) {
+            if(!(obj instanceof SportCar)) return false;
+            return acceleratingTime == ((SportCar)obj).acceleratingTime;
+        } else {
+            return false;
+        }
+    }
 }
