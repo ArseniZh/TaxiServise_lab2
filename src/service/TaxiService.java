2825b5786588c811cf6cdd38d9bcd71b10bc1d2f
ArseniZh <szhirnevich@mail.ru>
2022-12-05 19:24:25 +0300

    feat (TaxiService): add methods calculateTotalCost, sortByFuelConsumption, outputCars, findCarInRange

--- src/service/TaxiService.java
+++ src/service/TaxiService.java
@@ -6,5 +6,30 @@ import java.util.Collections;
 import java.util.Comparator;
 
 public class TaxiService {
+    public double calculateTotalCost(ArrayList<Car> taxiService) {
+        double totalCost = 0;
 
+        for (Car car : taxiService) {
+            totalCost += car.getCost();
+        }
+        return totalCost;
+    }
+
+    public void sortByFuelConsumption(ArrayList<Car> taxiService, Comparator fuelConsumptionComparator) {
+        Collections.sort(taxiService, fuelConsumptionComparator);// from min to max
+    }
+
+    public void outputCars(ArrayList<Car> taxiService) {
+        for (Car car : taxiService) {
+            System.out.print(car.getModel() + ",  ");
+        }
+    }
+
+    public void findCarInRange(ArrayList<Car> taxiService, double leftLimit, double rightLimit) {
+        for (Car car : taxiService) {
+            if ( (car.getMaxSpeed() > leftLimit) && (car.getMaxSpeed() < rightLimit) ) {
+                System.out.println(car.getModel());
+            }
+        }
+    }
 }
