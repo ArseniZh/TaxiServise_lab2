08c13e3fb5be4101cc567cdf1113db2cc9f98470
ArseniZh <szhirnevich@mail.ru>
2022-12-05 16:38:27 +0300

    refactor (Main): change objects parameters

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,12 @@ public class Main {
         double totalCost = 0;
         ArrayList<Car> taxiService = new ArrayList<>();
 
-        taxiService.add(new PassengerCar("Audi RS e-tron GT", 240, 2150, 120000, 3, 2));
+        taxiService.add(new PassengerCar("Kia Rio", 200, 2150, 80000, 3, 4));
         taxiService.add(new PassengerCar("Mercedes AMG C 43 Coupe", 190, 2000, 120000, 1.7,2));
         taxiService.add(new CargoCar("ГАЗ-3302", 140, 2100, 20000, 2.7, 1660));
         taxiService.add(new CargoCar("УАЗ 2206", 120, 1900, 9000, 2.4, 1110));
         taxiService.add(new PassengerCar("Audi A5", 210, 2300, 90000, 2.3, 2));
-        taxiService.add(new PassengerCar("BMW M8 competition", 263, 1900, 155000, 2.5,2));
+        taxiService.add(new SportCar("BMW M8 competition", 263, 1900, 155000, 2.5,2));
         taxiService.add(new SportCar("Koenigsegg Jesko Absolut", 472, 1320, 3800000, 4.0, 2.8));
 
         System.out.println(taxiService.get(0).toString() + "\n");
